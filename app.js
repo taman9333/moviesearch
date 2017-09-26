243fda124c23a987d2f66a6be90643adc6a7e36c
Abd El-Rahman Taman <abdelrahman.taman@hotmail.com>
2017-09-26 05:46:22 +0000

    final

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var request = require('request');
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(req, res){
     res.render('home');
